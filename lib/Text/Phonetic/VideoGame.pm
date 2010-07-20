e414a05c629ffb7658febb535852d2c5e806d5d6
Michael Hendricks <michael@ndrix.org>
2010-07-20 12:11:44 -0600

    Release v0.03

--- lib/Text/Phonetic/VideoGame.pm
+++ lib/Text/Phonetic/VideoGame.pm
@@ -8,7 +8,7 @@ use Roman ();
 use Lingua::EN::Inflect::Number qw( to_S );
 use List::MoreUtils qw( uniq );
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 my %ordinal = (
     '1st' => 'first',
     '2nd' => 'second',
